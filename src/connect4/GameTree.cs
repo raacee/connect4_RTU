a39cbbdea86e11ad986f64bbc23a7e17212bb935
racel <raceldamechli@outlook.fr>
2023-05-06 17:59:38 +0300

    Added GameTree and StateNode constructors and props & methods

--- src/connect4/GameTree.cs
+++ src/connect4/GameTree.cs
@@ -4,16 +4,42 @@ class GameTree
 {
     private StateNode _root;
 
-    public StateNode Root
+    private GameTree()
+    {
+        this._root = new StateNode(new Grid());
+    }
+
+    private StateNode Root
     {
         get { return _root; }
+    }
+
+    public static GameTree GenerateGameTree()
+    {
+        return new GameTree();
     }
 }
 
 class StateNode
 {
     private Grid _grid;
-    private List<StateNode> _children = new List(0);
+    private List<StateNode> _children;
+    private int _player;
+
+    private readonly Dictionary<int, ConsoleColor> _playerDict = new Dictionary<int, ConsoleColor>(
+        new KeyValuePair<int, ConsoleColor>[]
+        {
+            new KeyValuePair<int, ConsoleColor>(1, ConsoleColor.DarkYellow),
+            new KeyValuePair<int, ConsoleColor>(-1, ConsoleColor.DarkRed)
+        });
+
+    public StateNode(Grid grid)
+    {
+        this._grid = grid;
+        this._children = new List<StateNode>(0);
+        this._player = this._grid.TokenCount() % 2 == 0? 1 : -1;
+    }
+    
     public Grid Grid
     {
         get { return _grid; }
@@ -23,9 +49,25 @@ class StateNode
         get { return _children; }
     }
 
-    public void populateChildren()
+    public void PopulateChildren()
     {
-        
+        if(!this._grid.IsFull())
+        {
+            for (int i = 0; i < _grid.Tokens.GetLength(1); i++)
+            {
+                var gridCopy = _grid.Clone();
+                try
+                {
+                    _grid.InsertToken(i, new Token(_playerDict[this._player]));
+                }
+                catch (ArgumentException)
+                {
+                    continue;
+                }
+
+                _children.Add(new StateNode(gridCopy));
+            }
+        }
     }
 
 }
